87f78b1ccc061cc9f3ba93e96bf7a5d702f5b933
Alloh Nkefor Rodrick <allohnkeforrodrick@gmail.com>
2023-06-24 06:36:10 -0700

    More functions and nested loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * mul - multiply two numbers
+ * Description: multiply two numbers
+ * @num1: interger value
+ * @num2: interger value
+ *
+ * Return: result
+ * Example:
+ *      mul(4 * 5) ---> 20
+ */
+
+int mul(int num1, int num2)
+{
+	int result = 0;
+
+	result = num1 * num2;
+	return (result);
+}
