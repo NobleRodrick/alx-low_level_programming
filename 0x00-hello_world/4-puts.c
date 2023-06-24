42e95f453d9b616efcd648ea110b5b41c8e55206
Alloh Nkefor Rodrick <allohnkeforrodrick@gmail.com>
2023-06-24 06:29:40 -0700

    Hello World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,15 @@
-#include<stdio.h>
+#include <stdio.h>
+
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ * main - Entry point
+ *
+ * Description: A C program that prints with put function
+ *
+ * Return: Always 0 (Success)
+*/
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+	return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,15 @@
 #include<stdio.h>
+
 /**
- * * main - Entry point
- * *
- * * Return: Always 0 (Success)
- * */
+ * main - Entry point
+ *
+ * Description: A C program that prints using printf function.
+ *
+ * Return: Always 0 (Success)
+*/
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-		return (0);
+	return (0);
 }
