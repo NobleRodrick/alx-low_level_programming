cb0be69122618dd0033cc847f76dbe562d1e0705
Alloh Nkefor Rodrick <allohnkeforrodrick@gmail.com>
2023-07-05 17:26:12 -0700

    Recursions

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -6,7 +6,7 @@ int _strlen_recursion(char *s);
 /**
  * is_palindrome - checks if a string is a palindrome
  * @s: string to reverse
- * 
+ *
  * Return: 1 if it is, 0 it's not
  */
 int is_palindrome(char *s)
@@ -19,7 +19,7 @@ int is_palindrome(char *s)
 /**
  * _strlen_recursion - returns the length of a string
  * @s: string to calculate the length of
- * 
+ *
  * Return: length of the string
  */
 int _strlen_recursion(char *s)
@@ -34,7 +34,7 @@ int _strlen_recursion(char *s)
  * @s: string to check
  * @i: iterator
  * @len: length of the string
- * 
+ *
  * Return: 1 if palindrome, 0 if not
  */
 int check_pal(char *s, int i, int len)
