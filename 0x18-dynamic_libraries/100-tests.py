29e281719425f6fdf97e07cf454218df4295260e
Alloh Nkefor Rodrick <allohnkeforrodrick@gmail.com>
2023-10-16 20:11:06 -0700

    corrected

--- 0x18-dynamic_libraries/100-tests.py
+++ 0x18-dynamic_libraries/100-tests.py
@@ -1,11 +1,11 @@
 import random
 import ctypes
 
-target = ctypes.CDLL('./100-operations.so')
+cops = ctypes.CDLL('./100-operations.so')
 a = random.randint(-111, 111)
 b = random.randint(-111, 111)
-print("{} + {} = {}".format(a, b, target.add(a, b)))
-print("{} - {} = {}".format(a, b, target.sub(a, b)))
-print("{} x {} = {}".format(a, b, target.mul(a, b)))
-print("{} / {} = {}".format(a, b, target.div(a, b)))
-print("{} % {} = {}".format(a, b, target.mod(a, b)))
+print("{} + {} = {}".format(a, b, cops.add(a, b)))
+print("{} - {} = {}".format(a, b, cops.sub(a, b)))
+print("{} x {} = {}".format(a, b, cops.mul(a, b)))
+print("{} / {} = {}".format(a, b, cops.div(a, b)))
+print("{} % {} = {}".format(a, b, cops.mod(a, b)))
